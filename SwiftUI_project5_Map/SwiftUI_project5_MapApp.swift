0ba57c73a4018d09e2f29865a37f899453f97648
SeonJungKim <ksj083895@gmail.com>
2021-03-08 15:39:39 +0500

    Initial Commit

---
+++ SwiftUI_project5_Map/SwiftUI_project5_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_project5_MapApp.swift
+//  SwiftUI_project5_Map
+//
+//  Created by 김선중 on 2021/03/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_project5_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
